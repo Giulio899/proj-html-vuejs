3a4a91eba47ab67f24febf3d4bc34c15933aac78
Giulio <giulio.tavoni@gmail.com>
2021-02-17 15:53:44 +0100

    Menu and footer data taken from Vue

--- js/script.js
+++ js/script.js
@@ -23,6 +23,134 @@ var app = new Vue({
       url: '#'
     }
   ],
+  contact: [
+    {
+      type: "opening",
+      info: "Open Hours: Mon - Sat - 9:00 - 18:00",
+      icon: "fas fa-clock"
+    },
+    {
+      type: "telephone",
+      info: "+1 (305) 1234-5678",
+      icon: "fas fa-phone-alt"
+    },
+    {
+      type: "mail",
+      info: "hello@example.com",
+      icon: "fas fa-envelope"
+    },
+    {
+      type: "map",
+      info: "Main Avenue, 987",
+      icon: "fas fa-map-marker-alt"
+    }
+  ],
+  social: [
+    {
+      type: "facebook",
+      icon: "fab fa-facebook-f",
+      url: "#"
+    },
+    {
+      type: "twitter",
+      icon: "fab fa-twitter",
+      url: "#"
+    },
+    {
+      type: "linkedin",
+      icon: "fab fa-linkedin-in",
+      url: "#"
+    }
+  ],
+  footerInfo: [
+    {
+      category: "About",
+      subcategory: [
+        {
+          name: "The Company",
+          url: "#"
+        },
+        {
+          name: "Istitutional",
+          url: "#"
+        },
+        {
+          name: "Social & Events",
+          url: "#"
+        },
+        {
+          name: "Innovation",
+          url: "#"
+        },
+        {
+          name: "Environment",
+          url: "#"
+        },
+        {
+          name: "Technology",
+          url: "#"
+        }
+      ]
+    },
+    {
+      category: "Services",
+      subcategory: [
+        {
+          name: "Audit & Assurance",
+          url: "#"
+        },
+        {
+          name: "Financial Advisory",
+          url: "#"
+        },
+        {
+          name: "Analytics M&A",
+          url: "#"
+        },
+        {
+          name: "Middle Marketing",
+          url: "#"
+        },
+        {
+          name: "Legal Consulting",
+          url: "#"
+        },
+        {
+          name: "Regulatory Risk",
+          url: "#"
+        }
+      ]
+    },
+    {
+      category: "Support",
+      subcategory: [
+        {
+          name: "Responsibility",
+          url: "#"
+        },
+        {
+          name: "Terms of Use",
+          url: "#"
+        },
+        {
+          name: "About Cookies",
+          url: "#"
+        },
+        {
+          name: "Privacy Policy",
+          url: "#"
+        },
+        {
+          name: "Accessibility",
+          url: "#"
+        },
+        {
+          name: "Information",
+          url: "#"
+        }
+      ]
+    }
+  ],
     counter: 1
   },
   methods: {
